a0e19a57b904c67f29e0c148d66c88e2534c4f44
Artur Perwenis <artur.perwenis@tieto.com>
2018-01-30 09:49:06 +0100

    Issue #45. Stub made for problem solver. Next step is to make it do something.

---
+++ EulerProblems/Problems/EulerProblem34.cs
@@ -0,0 +1,23 @@
+﻿using QBits.Intuition.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EulerProblems.Problems
+{
+    /// <summary>
+    /// 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+    /// Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+    /// Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+    /// </summary>
+    [ProblemSolverClass("Problem 34", DisplayName = "Problem 34")]
+    public class EulerProblem34 : AbstractEulerProblem
+    {
+        protected override void Solve(out string answer)
+        {
+            answer = $"Computing...";
+        }
+
+    }
+}
