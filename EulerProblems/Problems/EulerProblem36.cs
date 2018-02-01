8f8f1a54553ee1f6bf89db620b58eef49e899f4c
Artur Perwenis <aperwenis@gmail.com>
2018-02-01 23:08:58 +0100

    Refactor problem4 to expose useful PalindromeManipulator method.

---
+++ EulerProblems/Problems/EulerProblem36.cs
@@ -0,0 +1,25 @@
+﻿using QBits.Intuition.Mathematics;
+using QBits.Intuition.Mathematics.Primes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EulerProblems.Problems
+{
+    /// <summary>
+    /// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+    /// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+    /// (Please note that the palindromic number, in either base, may not include leading zeros.)
+    /// </summary>
+    [ProblemSolverClass("Problem 36", DisplayName = "Problem 36")]
+    public class EulerProblem36 : AbstractEulerProblem
+    {
+        protected override void Solve(out string answer)
+        {
+            var range = Enumerable.Range(1, 999_999);
+            var test = 7;
+            answer = $"Computing... Last = {range.Max()}";
+        }
+    }
+}
---
+++ IntuitionLibrary/Collections/PalindromeManipulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QBits.Intuition.Collections
+{
+    /// <summary>
+    /// Class that can be used to search for palindromes in a sequence of items.
+    /// </summary>
+    public class PalindromeManipulator
+    {
+        /// <summary>
+        /// Simple search if the specified <paramref name="palindromeCandidate"/> is a palindrome.
+        /// </summary>
+        /// <param name="palindromeCandidate">String to check</param>
+        /// <returns>True if sequence is a palindrome. False otherwise.</returns>
+        public bool IsPalindrome(string palindromeCandidate)
+        {
+                if (string.IsNullOrWhiteSpace(palindromeCandidate)) return true;
+                var maxLen = (palindromeCandidate.Length / 2) + 1; //+1 to cover for middle digit in numbers with odd-number of digits.
+                var iterators = Enumerable.Range(0, maxLen);
+                var reverseSequence = palindromeCandidate.Reverse().ToList();
+                var returnValue = iterators.All(i => palindromeCandidate[i] == reverseSequence[i]); //Palindrome is when all digits from either direction match.
+                return returnValue;
+        }
+    }
+}
