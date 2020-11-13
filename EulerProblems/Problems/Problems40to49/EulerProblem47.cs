5a1fac45874ed8deeb443c4c06d650e69cb9aa9b
Artur Perwenis <aperwenis@hotmail.com>
2020-11-13 23:28:39 +0100

    Placeholder ror #84 created.

---
+++ EulerProblems/Problems/Problems40to49/EulerProblem47.cs
@@ -0,0 +1,37 @@
+﻿using QBits.Intuition.Collections;
+using QBits.Intuition.Mathematics;
+using QBits.Intuition.Mathematics.Primes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data.Odbc;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblems.Problems.Problems40to49
+{
+    /// <summary/>
+    [ProblemSolver("Distinct primes factors", "Problem 47",
+@"The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?")]
+    public class EulerProblem47 : AbstractEulerProblem
+    {
+        protected override void Solve(out string answer)
+        {
+            answer = $"Solution not created yet...";
+        }
+    }
+
+}
